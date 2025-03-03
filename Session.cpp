524dfab4692a0720e2ebfe915636a2e5f2396152
Georgiy <graubin1@mail.ru>
2025-03-04 00:23:56 +0300

    04.03--00.24

--- Session.cpp
+++ Session.cpp
@@ -11,7 +11,10 @@ Session::Session(tcp::socket&& socket, std::unordered_set<std::shared_ptr<Sessio
     sqlite3_open("info.db", &db);
     ok = sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS users(login TEXT, password TEXT);", 
         nullptr, nullptr, &error_mess);
+    ok = sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS chats(chat_name TEXT, user_name TEXT);",
+        nullptr, nullptr, &error_mess);
     pruf(ok, error_mess);
+    flag_sql = false;
 }
 
 void Session::async_write(std::string message)
@@ -35,7 +38,7 @@ void Session::async_read() {
 int Session::callback_author(void* x, int column, char** data, char** column_name)
 {
     Session* obj = static_cast<Session*>(x);
-    obj->found = 0;
+    obj->flag_sql = true;
     if (obj->password != std::string(data[1])) {
         obj->async_write("&NON_PASSWORD&");
     }
@@ -43,7 +46,29 @@ int Session::callback_author(void* x, int column, char** data, char** column_nam
         obj->async_write("&OK_PASSWORD&");
         std::cout << obj->socket.remote_endpoint().address().to_string() <<
             " зашёл как " << obj->name << std::endl;
+        int ok = sqlite3_exec(obj->db, std::string("SELECT chat_name FROM chats WHERE user_name='" + obj->name + "'").c_str(),
+            callback_chats, obj, &obj->error_mess);
+        obj->pruf(ok, obj->error_mess);
+        if (!obj->chats.empty()) {
+            for (const auto& i : obj->chats) {
+                obj->async_write("&LOADCHAT&" + i);
+            }
+        }
     }
+    return 0;
+}
+
+int Session::callback_chats(void* x, int column, char** data, char** column_name)
+{
+    Session* obj = static_cast<Session*>(x);
+    obj->chats.push_back(std::string(*data));
+    return 0;
+}
+
+int Session::callback_flag(void* x, int column, char** data, char** column_name)
+{
+    Session* obj = static_cast<Session*>(x);
+    obj->flag_sql = true;
     return 0;
 }
 
@@ -79,11 +104,29 @@ void Session::read_from_buffer(const error_code& error, std::size_t bytes)
     else if (line.starts_with("/help")) {
         _help();
     }
+    else if (line.starts_with("/users local")) {
+        _users_local();
+    }
     else if (line.starts_with("/users")) {
         _users();
     }
     else if (line.starts_with("&ENTER&")) {
         _enter(line);
+    }
+    else if (line.starts_with("&CREATECHAT&")) {
+        _create_chat(line);
+    }
+    else if (line.starts_with("&ADDUSERTOCHAT&")) {
+        _add_user_to_chat(line);
+    }
+    else if (line.starts_with("&OPENLOCALCHAT&")) {
+        _open_lockal_chat(line);
+    }
+    else if (line.starts_with("&OPENGLOBALCHAT&")) {
+        _open_global_chat();
+    }
+    else if (line.starts_with("&DELETECHAT&")) {
+        _delete_chat(line);
     }
     else {
         write_everyone(line);
@@ -92,24 +135,38 @@ void Session::read_from_buffer(const error_code& error, std::size_t bytes)
 
 void Session::write_everyone(std::string& line)
 {
-    for (auto& i : clients) {
-        if (i != shared_from_this())
-        {
-            i->async_write("<span style='color: green;'><b>"+name+":</b></span> " + line + '\n');
+    if (this_local_chat.empty()) {
+        for (auto& i : clients) {
+            if (i != shared_from_this() && i->this_local_chat.empty())
+            {
+                i->async_write("<span style='color: green;'><b>" + name + ":</b></span> " + line + '\n');
+            }
+        }
+    }
+    else {
+        for (auto& i : clients) {
+            if (i != shared_from_this() && i->this_local_chat == this_local_chat)
+            {
+                i->async_write("<span style='color: green;'><b>" + name + ":</b></span> " + line + '\n');
+            }
         }
     }
 
     async_read();
 }
 
+void Session::write_local_chat(std::string& line)
+{
+
+}
+
 void Session::autorization()
 {
-    found = 1;
     sql = "SELECT login, password FROM users WHERE login = '" + name + "';";
     ok = sqlite3_exec(db, sql.c_str(), callback_author, this, &error_mess);
     pruf(ok, error_mess);
 
-    if (found == 1) {
+    if (!flag_sql) {
         sql = std::format("INSERT INTO users (login, password) VALUES ('{}', '{}');",
             name, password);
         ok = sqlite3_exec(db, sql.c_str(), nullptr, nullptr, &error_mess);
@@ -120,6 +177,7 @@ void Session::autorization()
 
         async_write("&OK_PASSWORD&");
     }
+    flag_sql = false;
 }
 
 void Session::pruf(int ok, char* error)
@@ -139,7 +197,7 @@ void Session::_help()
 {
     std::string list = "Commands:<br>";
     list += "/users<br>";
-    list += "/exit";
+    list += "/users local";
 
     async_write(sm(list));
     async_read();
@@ -167,5 +225,86 @@ void Session::_enter(std::string line)
 
     clients.insert(shared_from_this());
 
+    async_read();
+}
+
+void Session::_create_chat(std::string line)
+{
+    line.erase(0, line.rfind('&') + 1);
+    ok = sqlite3_exec(db, std::string("SELECT chat_name FROM chats WHERE chat_name ='"+line+"';").c_str(),
+        callback_flag, this, &error_mess);
+
+    if (!flag_sql)
+    {
+        ok = sqlite3_exec(db, std::format("INSERT INTO chats (chat_name, user_name) VALUES ('{}', '{}');",
+            line, name).c_str(), nullptr, nullptr, &error_mess);
+        pruf(ok, error_mess);
+        async_write("&CHATCREATEOK&"+line);
+    }
+    else {
+        async_write("&CHATALREADYCREATED&");
+    }
+    flag_sql = false;
+    async_read();
+}
+
+void Session::_add_user_to_chat(std::string line)
+{
+    line.erase(0, line.rfind('&') + 1);
+    std::string user_name = line.substr(0, line.find('|'));
+    std::string chat_name = line.substr(line.find('|') + 1);
+    ok = sqlite3_exec(db, std::string("SELECT * FROM users WHERE login='" + user_name + "';").c_str(),
+        callback_flag, this, &error_mess);
+    pruf(ok, error_mess);
+    if (flag_sql) {
+        ok = sqlite3_exec(db, std::format("INSERT INTO chats (chat_name, user_name) VALUES ('{}', '{}');",
+            chat_name, user_name).c_str(), nullptr, nullptr, &error_mess);
+        pruf(ok, error_mess);
+        for (const auto i : clients) {
+            if (i->name == user_name) {
+                i->async_write("&LOADCHAT&"+chat_name);
+            }
+        }
+    }
+    else {
+        async_write("&USERNOINDB&");
+    }
+    flag_sql = false;
+    async_read();
+}
+
+void Session::_open_lockal_chat(std::string line)
+{
+    line.erase(0, line.rfind('&') + 1);
+    this_local_chat = line;
+    async_read();
+}
+
+void Session::_open_global_chat()
+{
+    this_local_chat.clear();
+    async_read();
+}
+
+void Session::_users_local()
+{
+    int count = 1;
+    std::string users = "Users in "+this_local_chat+":";
+    for (const auto& i : clients) {
+        if (i->this_local_chat == this_local_chat) {
+            users += "<br>" + std::to_string(count) + '.' + i->name;
+            count++;
+        }
+    }
+    async_write(sm(users));
+    async_read();
+}
+
+void Session::_delete_chat(std::string chat)
+{
+    chat.erase(0, chat.rfind('&') + 1);
+    ok = sqlite3_exec(db, std::string("DELETE FROM chats WHERE user_name='" + name + "' AND chat_name='" + chat + "';").c_str(),
+        nullptr, nullptr, &error_mess);
+    pruf(ok, error_mess);
     async_read();
 }
--- Session.hpp
+++ Session.hpp
@@ -8,6 +8,7 @@
 #include <unordered_set>
 #include <streambuf>
 #include "sqlite3.h"
+#include <set>
 
 class Session : public std::enable_shared_from_this<Session> {
 
@@ -23,13 +24,19 @@ protected:
 	char* error_mess;
 	int ok{}; 
 	static int callback_author(void* x, int column, char** data, char** column_name);
-	int found{}; //Флаг, найден ли пользователь в БД
+	static int callback_chats(void* x, int column, char** data, char** column_name);
+	static int callback_flag(void* x, int column, char** data, char** column_name);
+
+	bool flag_sql {false}; //Флаг, найден ли пользователь в БД
 
 	//Чтение из потока в буфер
 	void read_from_buffer(const boost::system::error_code& error, std::size_t bytes);
 
 	//Отправка сообщения всем поользователям
 	void write_everyone(std::string& line);
+
+	//Отправка сообщения в локальный чат
+	void write_local_chat(std::string& line);
 
 	//Авторизация
 	void autorization();
@@ -43,6 +50,12 @@ protected:
 	void _help();
 	void _users();
 	void _enter(std::string);
+	void _create_chat(std::string);
+	void _add_user_to_chat(std::string);
+	void _open_lockal_chat(std::string);
+	void _open_global_chat();
+	void _users_local();
+	void _delete_chat(std::string);
 
 public:
 	Session(boost::asio::ip::tcp::socket&& socket, std::unordered_set<std::shared_ptr<Session>>& clients);
@@ -50,6 +63,8 @@ public:
 	std::string name; //Имя пользователя
 	std::string role;
 	std::string password;
+	std::vector<std::string> chats;
+	std::string this_local_chat;
 
 	//Отправляет текущему пользователю сообщение
 	void async_write(std::string message);
